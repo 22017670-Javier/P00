9b46644af76d20e3ce5cf71fc95b8afa6f6bad2c
22017670 <22017670@DESKTOP-9VIPJHE>
2023-05-26 22:53:29 +0800

    first push to P00 repository in GitHub Javier

---
+++ P002/src/Calculator.java
@@ -0,0 +1,27 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 22017670, 26 May 2023 2:07:09 pm
+ */
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+}
